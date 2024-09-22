4503c64a8427466b197a5e6b907f2abe7d653662
Mohammad Nasser <mhmdnaser1572@gmail.com>
2024-09-22 13:59:20 +0300

    completed the employee class and connected succesfsully to the database MYSQL

--- src/main/java/com/nasser/emsbackend/entity/Employee.java
+++ src/main/java/com/nasser/emsbackend/entity/Employee.java
@@ -1,4 +1,30 @@
 package com.nasser.emsbackend.entity;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "emloyees")
 public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//automatically increment the primary key in db
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email_id", nullable = false, unique = true)//non nullable: cannot have null values + unique email
+    private String email;
 }
